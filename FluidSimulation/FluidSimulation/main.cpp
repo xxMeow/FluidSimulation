42a5d7d3ab86cee21d8e22cdb1205300c1858a04
xxMeow <xmx1025@gmail.com>
2019-12-01 19:30:15 +0900

    Environment Setting

---
+++ FluidSimulation/FluidSimulation/main.cpp
@@ -0,0 +1,125 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include <cmath>
+
+#include "Headers/Program.h"
+
+#define WIDTH 800
+#define HEIGHT 800
+
+/** Functions **/
+void processInput(GLFWwindow *window);
+
+/** Callback functions **/
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void mouse_button_callback(GLFWwindow *window, int button, int action, int mods);
+void cursor_pos_callback(GLFWwindow *window, double xpos, double ypos);
+
+/** Global **/
+// Window and world
+GLFWwindow *window;
+glm::vec3 bgColor(100.0/255, 50.0/255, 60.0/255);
+
+int main(int argc, const char * argv[])
+{
+    /** Prepare for rendering **/
+    // Initialize GLFW
+    glfwInit();
+    // Set OpenGL version number as 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // Use the core profile
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // MacOS is forward compatible
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    
+    /** Create a GLFW window **/
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Fluid Simulation", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    // Set the context of this window as the main context of current thread
+    glfwMakeContextCurrent(window);
+    
+    // Initialize GLAD : this should be done before using any openGL function
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD." << std::endl;
+        glfwTerminate(); // This line isn't in the official source code, but I think that it should be added here.
+        return -1;
+    }
+    
+    /** Register callback functions **/
+    // Callback functions should be registered after creating window and before initializing render loop
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetCursorPosCallback(window, cursor_pos_callback);
+    
+    /** Renderers **/
+    // Render program definitions
+    
+    glEnable(GL_DEPTH_TEST);
+    
+    /** Redering loop **/
+    while (!glfwWindowShouldClose(window))
+    {
+        /** Check for events **/
+        processInput(window);
+        
+        /** Set background clolor **/
+        glClearColor(bgColor.x, bgColor.y, bgColor.z, 1.0); // Set color value (R,G,B,A) - Set Status
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        /** -------------------------------- Simulation & Rendering -------------------------------- **/
+        
+        
+        
+        /** -------------------------------- Simulation & Rendering -------------------------------- **/
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents(); // Update the status of window
+    }
+
+    glfwTerminate();
+    
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+    
+    }
+    if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE)
+    {
+        
+    }
+}
+
+void cursor_pos_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    
+}
+
+void processInput(GLFWwindow *window)
+{
+    /** Keyboard control **/ // If key did not get pressed it will return GLFW_RELEASE
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
